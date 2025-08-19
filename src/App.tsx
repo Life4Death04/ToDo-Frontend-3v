0f50cc01587a7a6a932a97a27180d7d58b0d48dc
Santiago Rodriguez <santiagodrm@gmail.com>
2025-08-19 22:26:03 +0200

    component: taskTable 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import Login from './components/auth/Login'
+/* import Login from './components/auth/Login' */
 import Register from './components/auth/Register'
 import NotFound from './components/404NotFound/404NotFound'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import ToDo from './components/todo/ToDo'
-import {Sidebar} from './components/Layout/Sidebar'
+/* import {Sidebar} from './components/Layout/Sidebar' */
 import { TasksTable } from './components/Layout/TasksTable'
 
 const queryClient = new QueryClient({
--- src/components/Layout/TasksTable.tsx
+++ src/components/Layout/TasksTable.tsx
@@ -2,137 +2,71 @@ export function TasksTable(){
     return(
         <main className="px-2 pt-4 xsm:ml-12 lg:ml-64">
             <h1 className="font-bold mb-4 xsm:text-xl md:text-2xl lg:text-3xl">My Tasks</h1>
-            <ul className="flex flex-col gap-3 lg:gap-0">
-                {/* <li className="p-3 bg-white shadow-xl rounded-lg">
-                    <header className="flex justify-between items-end">
-                        <div className="flex gap-4 items-end">
-                            <button>
-                                <i className="fa-solid fa-square-check sm:text-lg md:text-xl"></i>
-                            </button>
-                            <h3 className="self-start sm:text-lg md:text-xl">Task Content</h3>
-                        </div>
-                        <button>
-                            <i className="fa-solid fa-trash-can sm:text-lg md:text-xl"></i>
-                        </button>
-                    </header>
-                    <p className="my-3 mb-4 text-black/50 sm:text-lg md:text-xl">Due: Date Task</p>
-                    <footer>
-                        <span className="bg-gray-200 px-3 py-1 rounded-2xl sm:text-lg md:text-xl">
-                            Status
-                        </span>
-                    </footer>
-                </li> */}
-                <li className="flex justify-between items-center bg-gray-100 p-2 text-gray-400">
-                    <div className="flex gap-3 items-center">
-                        <i className="fa-solid fa-square-check text-xl"></i>
-                        <span className="text-md">TASK NAME</span>
-                    </div>
-                    <div className="flex gap-30">
-                        <span className="text-md">
-                            DUE DATE
-                        </span>
-                        <span className="text-md">
-                            PRIORITY
-                        </span>
-                        <span className="text-md">
-                            STATUS
-                        </span>
-                    </div>
-                    <div className="">
-                        <span className="text-md">
-                            ACTIONS
-                        </span>
-                    </div>
-                </li>
-                <li className="flex justify-between items-center bg-white border-t-1 border-b-1 border-gray-400 py-4 px-2">
-                    <div className="flex gap-3 items-center">
-                        <i className="fa-solid fa-square-check text-xl"></i>
-                        <span className="text-md">TASK NAME</span>
-                    </div>
-                    <div className="flex gap-30">
-                        <span className="text-md">
-                            DUE DATE
-                        </span>
-                        <span className="text-md">
-                            PRIORITY
-                        </span>
-                        <span className="text-md">
-                            STATUS
-                        </span>
-                    </div>
-                    <div className="">
-                        <span className="text-md">
-                            ACTIONS
-                        </span>
-                    </div>
-                </li>
-                <li className="flex justify-between items-center bg-white border-t-1 border-b-1 border-gray-400 py-4 px-2">
-                    <div className="flex gap-3 items-center">
-                        <i className="fa-solid fa-square-check text-xl"></i>
-                        <span className="text-md">TASK NAME</span>
-                    </div>
-                    <div className="flex gap-30">
-                        <span className="text-md">
-                            DUE DATE
-                        </span>
-                        <span className="text-md">
-                            PRIORITY
-                        </span>
-                        <span className="text-md">
-                            STATUS
-                        </span>
-                    </div>
-                    <div className="">
-                        <span className="text-md">
-                            ACTIONS
-                        </span>
-                    </div>
-                </li>
-                <li className="flex justify-between items-center bg-white border-t-1 border-b-1 border-gray-400 py-4 px-2">
-                    <div className="flex gap-3 items-center">
-                        <i className="fa-solid fa-square-check text-xl"></i>
-                        <span className="text-md">TASK NAME</span>
-                    </div>
-                    <div className="flex gap-30">
-                        <span className="text-md">
-                            DUE DATE
-                        </span>
-                        <span className="text-md">
-                            PRIORITY
-                        </span>
-                        <span className="text-md">
-                            STATUS
-                        </span>
-                    </div>
-                    <div className="">
-                        <span className="text-md">
-                            ACTIONS
-                        </span>
-                    </div>
-                </li>
-                <li className="flex justify-between items-center bg-white border-t-1 border-b-1 border-gray-400 py-4 px-2">
-                    <div className="flex gap-3 items-center">
-                        <i className="fa-solid fa-square-check text-xl"></i>
-                        <span className="text-md">TASK NAME</span>
-                    </div>
-                    <div className="flex gap-30">
-                        <span className="text-md">
-                            DUE DATE
-                        </span>
-                        <span className="text-md">
-                            PRIORITY
-                        </span>
-                        <span className="text-md">
-                            STATUS
-                        </span>
-                    </div>
-                    <div className="">
-                        <span className="text-md">
-                            ACTIONS
-                        </span>
-                    </div>
-                </li>
+            <header className="items-center bg-gray-100 px-4 py-2 text-gray-400 border-gray-400 border-b-1 font-bold xsm:hidden lg:flex">
+                <span className="flex-[2]">Task Name</span>
+                <span className="flex-1">Due Date</span>
+                <span className="flex-1">Priority</span>
+                <span className="flex-1">Status</span>
+                <span className="w-20 text-right">Actions</span>
+            </header>
+            <ul className="flex flex-col gap-3 lg:gap-0 lg:divide-y lg:divide-gray-200">
+                <TaskItem
+                    content="Task 1"
+                    dueDate="2023-10-01"
+                    priority="High"
+                    status="In Progress"
+                />
+                <TaskItem
+                    content="Task 2"
+                    dueDate="2023-10-02"
+                    priority="Medium"
+                    status="Completed"
+                />
             </ul>
         </main>
+    );
+}
+
+type TaskItemProps = {
+    content: string,
+    dueDate: string,
+    priority: string,
+    status: string,
+}
+
+function TaskItem({content, dueDate, priority, status}: TaskItemProps){
+    return(
+        <li className="lg:flex lg:gap-3 bg-white lg:border-b-1 lg:border-gray-400 xsm:p-3 py-3 px-4 xsm:shadow-xl lg:shadow-none xsm:rounded-lg lg:rounded-none">
+            <div className="flex gap-2 items-center flex-[2]">
+                {/* <i className="fa-solid fa-square-check text-orange text-xl"></i> */}
+                {/* <i className="fa-regular fa-square xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i> */}
+                {status === 'Completed' ? <i className="fa-solid fa-square-check text-orange text-xl"></i> : <i className="fa-regular fa-square xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i>}
+                <span className="xsm:text-sm md:text-base lg:text-lg overflow text-wrap">
+                    {content}
+                </span>
+                <button className="text-gray-400 hover:text-orange ">
+                    <i className="fa-solid fa-pen xsm:text-base lg:text-lg"></i>
+                </button>
+                <div className="ml-auto mt-auto text-center w-20 lg:self-center lg:hidden">
+                    <i className="fa-regular fa-trash-can xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i>
+                </div>
+            </div>
+            <div className="flex xsm:flex-col lg:flex-row lg:items-center xsm:gap-3 xsm:mt-4 lg:mt-0 flex-[3]">
+                <span className="xsm:text-xs md:text-sm lg:text-base lg:flex-1">
+                    {dueDate}
+                </span>
+                <span className="xsm:text-xs md:text-sm lg:text-base lg:flex-1 text-red-500 font-bold">
+                    {priority}
+                </span>
+                <div className="lg:flex-1"> 
+                    <span className="xsm:text-xs md:text-sm lg:text-base xsm:w-fit bg-amber-100 px-2 py-1 rounded-xl text-amber-500 font-bold">
+                        {status}
+                    </span>
+                </div>
+            </div>
+            <div className="ml-auto mt-auto text-center w-20 lg:block lg:self-center xsm:hidden">
+                <i className="fa-regular fa-trash-can xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i>
+            </div>
+        </li>
     );
 }
