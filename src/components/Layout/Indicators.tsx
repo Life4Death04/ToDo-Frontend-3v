116538f785bb1a000d013729dc98f14acb9a983c
Santiago Rodriguez <santiagodrm@gmail.com>
2025-08-20 16:01:01 +0200

    component: All Component Details Polished

--- src/components/Layout/Indicators.tsx
+++ src/components/Layout/Indicators.tsx
@@ -1,11 +1,22 @@
+import type React from "react";
+
 export function IndicatorsPanel(){
     return (
-        <div className="flex items-center flex-wrap justify-center p-6 gap-8 rounded-lg shadow-lg w-fit mx-auto">
-        <RadialIndicator textIndicator="Tasks Completed" totalTasks={20} completedTasks={15}/>
-        <TextIndicator textIndicator="Reminder" totalItems={3} iconIndicator="fa-solid fa-clock"/>
-        <TextIndicator textIndicator="Archived Tasks" totalItems={3} iconIndicator="fa-solid fa-archive"/>
-        </div>
+        <section className="flex items-center flex-wrap justify-center p-6 gap-8 rounded-lg w-fit mx-auto mt-8">
+          <RadialIndicator textIndicator="Tasks Completed" totalTasks={20} completedTasks={15}/>
+          <TextIndicator textIndicator="Reminder" totalItems={3} iconIndicator="fa-solid fa-clock"/>
+          <TextIndicator textIndicator="Archived Tasks" totalItems={3} iconIndicator="fa-solid fa-archive"/>
+        </section>
     );
+}
+
+function IndicatorCard({children}: {children: React.ReactNode}){
+  return(
+    <div className="flex flex-col items-center justify-center bg-amber-200/50 rounded-2xl shadow-md 
+    p-6 w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64">
+      {children}
+    </div>
+  )
 }
 
 type TextIndicatorProps = {
@@ -16,20 +27,19 @@ type TextIndicatorProps = {
 
 function TextIndicator({textIndicator, totalItems, iconIndicator}: TextIndicatorProps){
     return(
-    <div className="flex flex-col items-center justify-center bg-amber-200/50 rounded-2xl shadow-md 
-    p-6 w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64">
-      <div className="relative flex items-center justify-center bg-white xsm:p-4 md:p-6 lg:p-8  rounded-full">
-        <i className={`${iconIndicator} sm:text-xl md:text-2xl lg:text-3xl`}></i>
-      </div>
+      <IndicatorCard>
+        <div className="relative flex items-center justify-center bg-white xsm:p-4 md:p-6 lg:p-8  rounded-full">
+          <i className={`${iconIndicator} sm:text-xl md:text-2xl lg:text-3xl`} aria-hidden:true></i>
+        </div>
 
-      <h2 className="mt-4 mb-2 text-sm sm:text-base md:text-lg lg:text-xl font-semibold text-gray-800">
-        {textIndicator}
-      </h2>
+        <h2 className="mt-4 mb-2 text-sm sm:text-base md:text-lg lg:text-xl font-semibold text-gray-800">
+          {textIndicator}
+        </h2>
 
-      <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-orange font-bold">
-        {totalItems}
-      </p>
-    </div>
+        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-orange font-bold">
+          {totalItems}
+        </p>
+      </IndicatorCard>
     );
 }
 
@@ -40,7 +50,7 @@ type RadialIndicatorProps = {
 }
 
 export function RadialIndicator({textIndicator, totalTasks, completedTasks}:RadialIndicatorProps) {
-  const percentage = Math.round((completedTasks / totalTasks) * 100);
+  const percentage = totalTasks > 0 ? Math.round((completedTasks / totalTasks) * 100) : 0; //Safeguard in case totaltask equals 0
 
   // Circle math
   const radius = 45;
@@ -49,9 +59,7 @@ export function RadialIndicator({textIndicator, totalTasks, completedTasks}:Radi
   const offset = circumference - (percentage / 100) * circumference;
 
   return (
-    <div className="flex flex-col items-center justify-center bg-amber-200/50 rounded-2xl shadow-md 
-      p-4 w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64">
-      
+    <IndicatorCard>
       {/* Radial Progress (SVG) */}
       <div className="relative flex items-center justify-center">
         <svg
@@ -96,6 +104,6 @@ export function RadialIndicator({textIndicator, totalTasks, completedTasks}:Radi
       <p className="text-xs sm:text-sm md:text-base lg:text-lg text-gray-600">
         {percentage}%
       </p>
-    </div>
+    </IndicatorCard>
   );
 }
--- src/components/Layout/TasksTable.tsx
+++ src/components/Layout/TasksTable.tsx
@@ -1,11 +1,9 @@
-import { type JSX } from "react";
-
 export function TasksTable(){
     return(
-        <main className="px-2 pt-4">
+        <section className="px-2 pl-4 pt-4">
             <h1 className="font-bold mb-4 xsm:text-xl md:text-2xl lg:text-3xl">My Tasks</h1>
-            <header className="items-center bg-gray-100 px-4 py-2 text-gray-400 border-gray-400 border-b-1 font-bold xsm:hidden lg:flex">
-                <span className="flex-[2]">Task Name</span>
+            <header className="items-center bg-gray-100 px-4 py-2 text-gray-400 border-gray-400 border-b font-bold xsm:hidden lg:flex">
+                <span className="flex-2">Task Name</span> {/* //flex-[2] */}
                 <span className="flex-1">Due Date</span>
                 <span className="flex-1">Priority</span>
                 <span className="flex-1">Status</span>
@@ -25,7 +23,7 @@ export function TasksTable(){
                     status="Completed"
                 />
             </ul>
-        </main>
+        </section>
     );
 }
 
@@ -38,14 +36,12 @@ type TaskItemProps = {
     status: CurrentStatusTask,
 }
 
-interface CheckStatusIcons {
-    check: JSX.Element,
-    uncheck: JSX.Element
-}
-
-const checkStatusIcons: CheckStatusIcons = {
-    check: <i className="fa-solid fa-square-check text-orange xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i>,
-    uncheck: <i className="fa-regular fa-square xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i>
+function getCheckIcon(isChecked: boolean){
+    return isChecked ? (
+        <i className="fa-solid fa-square-check text-orange xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer" aria-hidden:true></i>
+    ) : (
+        <i className="fa-regular fa-square xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer" aria-hidden:true></i>
+    )
 }
 
 function TaskItem({content, dueDate, priority, status}: TaskItemProps){
@@ -53,17 +49,17 @@ function TaskItem({content, dueDate, priority, status}: TaskItemProps){
         <li className="lg:flex lg:gap-3 bg-white lg:border-b-1 lg:border-gray-400 xsm:p-3 py-3 px-4 xsm:shadow-xl lg:shadow-none xsm:rounded-lg lg:rounded-none">
             <div className="flex gap-2 items-center flex-[2]">
                 <button>
-                    {status === 'Completed' ? checkStatusIcons.check : checkStatusIcons.uncheck}
+                    {getCheckIcon(status === 'Completed')}
                 </button>
-                <span className="xsm:text-sm md:text-base lg:text-lg overflow text-wrap">
+                <span className="xsm:text-sm md:text-base lg:text-lg overflow break-words">
                     {content}
                 </span>
                 <Button iconStyle="fa-solid fa-pen" buttonStyle="text-gray-400"></Button>
                 <div className="ml-auto mt-auto text-center w-20 lg:self-center lg:hidden">
-                    <i className="fa-regular fa-trash-can xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i>
+                    <i className="fa-regular fa-trash-can xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer" aria-hidden:true></i>
                 </div>
             </div>
-            <div className="flex xsm:flex-col lg:flex-row lg:items-center xsm:gap-3 xsm:mt-4 lg:mt-0 flex-[3]">
+            <div className="flex xsm:flex-col lg:flex-row lg:items-center xsm:gap-3 xsm:mt-2 lg:mt-0 flex-[3]">
                 <span className="xsm:text-xs md:text-sm lg:text-base lg:flex-1">
                     {dueDate}
                 </span>
@@ -77,7 +73,6 @@ function TaskItem({content, dueDate, priority, status}: TaskItemProps){
                 </div>
             </div>
             <div className="ml-auto mt-auto text-center w-20 lg:block lg:self-center xsm:hidden">
-                {/* <i className="fa-regular fa-trash-can xsm:text-base lg:text-lg hover:text-orange hover:cursor-pointer"></i> */}
                 <Button iconStyle="fa-regular fa-trash-can"></Button>
             </div>
         </li>
@@ -93,7 +88,7 @@ type ButtonProps = {
 function Button({ onClick, iconStyle, buttonStyle }: ButtonProps){
     return(
         <button className={`hover:cursor-pointer hover:text-orange px-4 py-2 ${buttonStyle}`} onClick={onClick}>
-            <i className={`${iconStyle} xsm:text-base lg:text-lg`}></i>
+            <i className={`${iconStyle} xsm:text-base lg:text-lg`} aria-hidden:true></i>
         </button>
     );
 }
