8553cae1c71fc877d5a8cfbc3ceb1a1f770427b4
Santiago Rodriguez <santiagodrm@gmail.com>
2025-08-26 22:45:46 +0200

    To be honest, I dont know wh

---
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -0,0 +1,19 @@
+import { ButtonIcon } from "../Common/CommonComponents";
+
+type ErrorModalProps = {
+    errorType?: string,
+    errorMessage?: string
+}
+const errorType:string = 'Error registering user';
+const errorMessage:string = 'User already exists';
+
+export function ErrorModal(/* { errorType, errorMessage }: ErrorModalProps */){
+
+    return(
+        <div className="flex items-center justify-center px-2 py-4 ml-auto mt-2 xsm:w-xs sm:w-fit rounded-2xl shadow-lg bg-red-200 border border-red-500">
+            <i className="fa-solid fa-circle-exclamation text-red-500 text-2xl md:text-3xl bg-transparent "></i>
+            <p className="bg-transparent mx-2 text-gray-500 xsm:text-sm md:text-base">{errorType}: {errorMessage}</p>
+            <ButtonIcon iconStyle="fa-solid fa-x" buttonStyle="text-red-400 bg-transparen" />
+        </div>
+    );
+}
